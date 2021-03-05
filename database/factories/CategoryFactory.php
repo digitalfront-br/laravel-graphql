0a5b783bb216f8a9ef9067d977953c93fa711e7a
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-03-05 10:06:59 -0300

    [Excel]  category import + seed factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Imports\CategoryImport;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Maatwebsite\Excel\Facades\Excel;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        Excel::import(new CategoryImport, storage_path('sheets/categorias.xls'));
+        return [];
+    }
+}
