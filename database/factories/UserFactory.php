e4725fd4a98378f01e0256d1ba96d2c8828a56ef
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-03-05 09:49:46 -0300

    [User] user factory and seed data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -22,13 +23,22 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
+        $users = array(
+            ['phone' => '31987324565', 'name' => 'Thiago Augusto', 'email' => 'thiago.digitalfront@gmail.com'],
+            ['phone' => '31986435111', 'name' => 'Daniel Nunes', 'email' => 'danielpnunes@gmail.com'],
+            ['phone' => '31998420055', 'name' => 'José Ricardo', 'email' => 'jrer@uol.com.br']
+        );
+        foreach ($users as $user) {
+            User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'phone' => $user['phone'],
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+        return [];
     }
 
     /**
